7f937b77ea2fbeb88e33b0d421ffb4754920e32c
Furkan Karaketir <furkankaraketir2005@gmail.com>
2022-05-29 11:44:51 +0300

    Rotating Cube View Added

--- graphicEngine/graphicEngine.cpp
+++ graphicEngine/graphicEngine.cpp
@@ -18,6 +18,13 @@ struct mesh
 	vector<triangle> tris;
 };
 
+struct mat4x4
+{
+
+	float m[4][4] = { 0 };
+
+};
+
 
 class graphicEngine :public olcConsoleGameEngine {
 public:
@@ -25,14 +32,173 @@ public:
 		m_sAppName = L"3D Demo";
 	}
 
+
+private:
+	mesh meshCube;
+	mat4x4 matProj;
+	float fTheta;
+
+
+	void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m) {
+		o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+		o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+		o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+		float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+		if (w != 0) {
+			o.x /= w;
+			o.y /= w;
+			o.z /= w;
+		}
+	}
+
+
+
 public:
 	bool OnUserCreate() override {
+
+		meshCube.tris = {
+
+			//South
+
+			{0.0f,0.0f,0.0f, 0.0f,1.0f,0.0f, 1.0f,1.0f,0.0f},
+			{0.0f,0.0f,0.0f, 1.0f,1.0f,0.0f, 1.0f,0.0f,0.0f},
+
+			//East
+			{1.0f,0.0f,0.0f, 1.0f,1.0f,0.0f, 1.0f,1.0f,1.0f},
+			{1.0f,0.0f,0.0f, 1.0f,1.0f,1.0f, 1.0f,0.0f,1.0f},
+
+			//North
+			{1.0f,0.0f,1.0f, 1.0f,1.0f,1.0f, 0.0f,1.0f,1.0f},
+			{1.0f,0.0f,1.0f, 0.0f,1.0f,1.0f, 0.0f,0.0f,1.0f},
+
+			//West
+			{0.0f,0.0f,1.0f, 0.0f,1.0f,1.0f, 0.0f,1.0f,0.0f},
+			{0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f, 0.0f,0.0f,0.0f},
+
+			//Top
+			{0.0f,1.0f,0.0f, 0.0f,1.0f,1.0f, 1.0f,1.0f,1.0f},
+			{0.0f,1.0f,0.0f, 1.0f,1.0f,1.0f, 1.0f,1.0f,0.0f},
+
+
+			//Bottom
+			{1.0f,0.0f,1.0f, 0.0f,0.0f,1.0f, 0.0f,0.0f,0.0f},
+			{1.0f,0.0f,1.0f, 0.0f,0.0f,0.0f, 1.0f,0.0f,0.0f},
+
+
+
+		};
+
+		// Projection Matrix
+
+		float fNear = 0.1f;
+		float fFar = 1000.0f;
+		float fFov = 90.0f;
+		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+
+		matProj.m[0][0] = fAspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar / (fFar - fNear);
+		matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+		matProj.m[2][3] = 1.0f;
+		matProj.m[3][3] = 0.0f;
+
+
+
 
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override {
 
+
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+		mat4x4 matRotZ, matRotX;
+		fTheta += 1.0f * fElapsedTime;
+
+		// Rotation Z
+		matRotZ.m[0][0] = cosf(fTheta);
+		matRotZ.m[0][1] = sinf(fTheta);
+		matRotZ.m[1][0] = -sinf(fTheta);
+		matRotZ.m[1][1] = cosf(fTheta);
+		matRotZ.m[2][2] = 1;
+		matRotZ.m[3][3] = 1;
+
+		// Rotation X
+		matRotX.m[0][0] = 1;
+		matRotX.m[1][1] = cosf(fTheta * 0.5f);
+		matRotX.m[1][2] = sinf(fTheta * 0.5f);
+		matRotX.m[2][1] = -sinf(fTheta * 0.5f);
+		matRotX.m[2][2] = cosf(fTheta * 0.5f);
+		matRotX.m[3][3] = 1;
+
+
+
+		//Draw Tringles
+
+		for (auto tri : meshCube.tris) {
+			triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
+
+			triTranslated = tri;
+			triTranslated.p[0].z = tri.p[0].z + 3.0f;
+			triTranslated.p[1].z = tri.p[1].z + 3.0f;
+			triTranslated.p[2].z = tri.p[2].z + 3.0f;
+
+
+
+			// Rotate in Z-Axis
+			MultiplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
+			MultiplyMatrixVector(tri.p[1], triRotatedZ.p[1], matRotZ);
+			MultiplyMatrixVector(tri.p[2], triRotatedZ.p[2], matRotZ);
+
+			// Rotate in X-Axis
+			MultiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
+
+			// Offset into the screen
+			triTranslated = triRotatedZX;
+			triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
+			triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
+			triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
+
+			// Project triangles from 3D --> 2D
+			MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+			MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+			MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+
+
+			//Scale Into View
+
+			triProjected.p[0].x += 1.0f;
+			triProjected.p[0].y += 1.0f;
+
+			triProjected.p[1].x += 1.0f;
+			triProjected.p[1].y += 1.0f;
+
+			triProjected.p[2].x += 1.0f;
+			triProjected.p[2].y += 1.0f;
+
+			triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[0].y *= 0.5f * (float)ScreenWidth();
+
+			triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[1].y *= 0.5f * (float)ScreenWidth();
+
+			triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[2].y *= 0.5f * (float)ScreenWidth();
+
+
+
+
+
+			DrawTriangle(
+				triProjected.p[0].x, triProjected.p[0].y,
+				triProjected.p[1].x, triProjected.p[1].y,
+				triProjected.p[2].x, triProjected.p[2].y, PIXEL_SOLID, FG_WHITE);
+		}
 
 		return true;
 	}
